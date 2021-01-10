33f1d9948de97e057859bf97cdaa3ebea40ca43f
Lupe <lupemaydana@gmail.com>
2021-01-09 22:12:52 -0400

    chore: Remove static variable local

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,8 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    ("/c/Users/lupec/code-projects/poll-evaluation/polls/static"),
-]
 
 django_heroku.settings(locals())
