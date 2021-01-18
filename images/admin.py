340d8da0e60f369c0f1a70422f977d19266ccafb
Lupe <lupemaydana@gmail.com>
2021-01-17 21:29:28 -0400

    feat: Show images in admin

--- images/admin.py
+++ images/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Car
 
+
+class CarAdmin(admin.ModelAdmin):
+
+    readonly_fields = ["photo", "headshot_image"]
+
+    def headshot_image(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.photo.url,
+            width=obj.photo.width,
+            height=obj.photo.height,
+        )
+        )
+
+
 # Register your models here.
-admin.site.register(Car)
+admin.site.register(Car, CarAdmin)
